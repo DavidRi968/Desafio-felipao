59e5ad3bb02601c86cfea6deac673eb1311b6371
David <davirdlessa@gmail.com>
2024-12-16 15:34:52 -0300

    Finalizado

---
+++ index.js
@@ -0,0 +1,25 @@
+let nome = "Lenda"
+let xp = 1000
+let nivel;
+
+if (xp <=1000) {
+    nivel = "Ferro"
+} else if (xp <=2000){
+    nivel = "Broze"
+} else if (xp <=5000){
+    nivel = "Prata"
+} else if (xp <=7000){
+    nivel = "Ouro"
+} else if (xp <=8000){
+    nivel = "Platina"
+}else if (xp <=9000){
+    nivel = "Ascendente"
+} else if (xp <=10000){
+    nivel = "Imortal"
+} else if (xp >=10001){
+    nivel = "Radiante"
+}
+console.log("Nome do Heroi:" + nome + " esta no nivel:" + nivel)
+
+
+
